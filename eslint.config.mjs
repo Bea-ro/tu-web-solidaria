9aef24021392d706552d6dd1d98f74cf93d30313
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2024-11-05 21:53:12 +0100

    install and config prettierrc y eslint and add global styles

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+    plugins: { prettier: prettierPlugin },
+    rules: { 'prettier/prettier': 'error' }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig
+];
