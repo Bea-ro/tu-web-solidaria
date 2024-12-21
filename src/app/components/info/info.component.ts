6c6eef59e168db64f3461b79d257b6d285c6b0d3
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2024-12-21 08:59:16 +0100

    repeat info-component

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ButtonComponent } from '../button/button.component';
+import { cardTexts } from '../../data/cardTexts';
+import { CardText } from '../card/card.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [CardComponent, ButtonComponent],
+  imports: [CardComponent, ButtonComponent, CommonModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
-export class InfoComponent {}
+export class InfoComponent {
+  cardTexts: CardText[][] = cardTexts;
+}
---
+++ src/app/data/cardTexts.ts
@@ -0,0 +1,21 @@
+export const cardTexts = [
+  [
+    { type: 'small-text', text: 'Tu página sirve como ' },
+    { type: 'mid-text', text: 'punto de encuentro con tus ' },
+    { type: 'remarked-text', text: 'beneficiarios' },
+    { type: 'small-text', text: '24/7' }
+  ],
+  [
+    {
+      type: 'small-text',
+      text: 'Tendrás una carta de presentación a '
+    },
+    { type: 'remarked-text', text: 'colaboradores ' },
+    { type: 'mid-text', text: 'potenciales' }
+  ],
+  [
+    { type: 'small-text', text: 'Es una evidencia más de tu ' },
+    { type: 'remarked-text', text: 'transparencia ' },
+    { type: 'mid-text', text: 'y te facilitará el acceso a subvenciones' }
+  ]
+];
